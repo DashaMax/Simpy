f5a273c4cc4ae9b5fb6c9eebdc320ccf33ab97e0
Dasha Max <110603766+DashaMax@users.noreply.github.com>
2023-06-24 20:54:04 +0300

    add pages (create index.html)

---
+++ pages/static/script.js
@@ -0,0 +1,24 @@
+/* let ul = document.querySelector('.nav-menu ul');
+let dot = document.querySelector('#dot');
+
+if(window.innerWidth <= 600){
+    ul.style.display = 'none';
+    dot.style.display = 'flex';
+}
+else{
+    ul.style.display = 'flex';
+    dot.style.display = 'none';
+}
+
+window.addEventListener('resize', function(){
+
+    if(window.innerWidth <= 600){
+        ul.style.display = 'none';
+        dot.style.display = 'flex';
+    }
+    else{
+        ul.style.display = 'flex';
+        dot.style.display = 'none';
+    }
+
+}); */
