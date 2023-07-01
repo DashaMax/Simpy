ace9af95d5f304b778ff87a473bdabe7b4123e50
Dasha Max <110603766+DashaMax@users.noreply.github.com>
2023-07-01 23:55:29 +0300

    add books.html and update pages/

--- pages/static/script.js
+++ pages/static/script.js
@@ -1,24 +1,21 @@
-/* let ul = document.querySelector('.nav-menu ul');
 let dot = document.querySelector('#dot');
+let closeMenu = document.querySelector('.close-menu');
+let closeMenuH3 = document.querySelector('.close-menu h3');
 
-if(window.innerWidth <= 600){
-    ul.style.display = 'none';
-    dot.style.display = 'flex';
-}
-else{
-    ul.style.display = 'flex';
-    dot.style.display = 'none';
-}
+dot.onclick = function(){
+    closeMenu.style.display = 'block';
+};
 
-window.addEventListener('resize', function(){
+closeMenuH3.onclick = function(){
+    closeMenu.style.display = 'none';
+};
 
-    if(window.innerWidth <= 600){
-        ul.style.display = 'none';
-        dot.style.display = 'flex';
+let bookAdd = document.querySelectorAll('.add');
+
+for(let i=0; i<bookAdd.length; i++){
+    bookAdd[i].onclick = function(){
+        bookAdd[i].innerHTML = 'Добавлена';
+        bookAdd[i].classList.remove('add');
+        bookAdd[i].classList.add('add-yet');
     }
-    else{
-        ul.style.display = 'flex';
-        dot.style.display = 'none';
-    }
-
-}); */
+};
