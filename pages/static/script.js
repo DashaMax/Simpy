30c20bec3ea6c3f7f41cb03e3508bd8f0ba150f6
Dasha Max <110603766+DashaMax@users.noreply.github.com>
2023-07-08 17:08:38 +0300

    update pages/

--- pages/static/script.js
+++ pages/static/script.js
@@ -2,6 +2,10 @@ let dot = document.querySelector('#dot');
 let closeMenu = document.querySelector('.close-menu');
 let closeMenuH3 = document.querySelector('.close-menu h3');
 
+let butAdd = document.querySelector('.add-form');
+let addForm = document.querySelector('.block-back');
+let blockH2 = document.querySelector('.block-back h2');
+
 dot.onclick = function(){
     closeMenu.style.display = 'block';
 };
@@ -18,4 +22,12 @@ for(let i=0; i<bookAdd.length; i++){
         bookAdd[i].classList.remove('add');
         bookAdd[i].classList.add('add-yet');
     }
+};
+
+butAdd.onclick = function(){
+    addForm.style.display = 'block';
+};
+
+blockH2.onclick = function(){
+    addForm.style.display = 'none';
 };
